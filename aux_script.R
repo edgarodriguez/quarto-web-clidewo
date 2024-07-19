4f95a052903921a3b081e7e10344c09288c3e6af
Edgar Rodríguez-Huerta <edgarodriguez@outlook.com>
2024-07-19 15:23:51 +0100

    te

--- aux_script.R
+++ aux_script.R
@@ -2,6 +2,7 @@ parent_dir <- withr::local_tempdir()
 project_dir <- "blop"
 babelquarto::quarto_multilingual_website(parent_dir = parent_dir, project_dir = project_dir)
 
+quarto_version()
 babelquarto::render_website()
 
 quarto add quarto-ext/fontawesome
