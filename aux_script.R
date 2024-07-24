fbb02509c6f334b5212e5c2278c933ec82224608
Edgar Rodríguez-Huerta <edgarodriguez@outlook.com>
2024-07-24 10:30:27 +0100

    Testing with new repository

--- aux_script.R
+++ aux_script.R
@@ -2,6 +2,12 @@ parent_dir <- withr::local_tempdir()
 project_dir <- "blop"
 babelquarto::quarto_multilingual_website(parent_dir = parent_dir, project_dir = project_dir)
 
+remotes::install_github("EllaKaye/distilltools")
+remotes::install_github("ropensci/RefManageR")
+install.packages("bib2df")
+
+base::library(RefManageR)
+base::library(distilltools)
 quarto_version()
 babelquarto::render_website()
 
